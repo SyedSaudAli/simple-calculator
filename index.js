07fb1cce3a69aed5cfa548e38d7cce3717c7eb06
Syed Saud Ali <160472981+SyedSaudAli@users.noreply.github.com>
2024-03-20 01:16:07 +0500

    My first online assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const asnwer = await inquirer.prompt([
+    { message: "Enter_first_number:", type: "number", name: "FirstNumber" },
+    { message: "Enter_second_number", type: "number", name: "SecondNumber" },
+    {
+        message: "Please select one operator to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition (+)", "Subtraction (-)", "Multiplication (x)", "Division (÷)", "Modulus (%)", "Exponentiation (xx)"],
+    },
+]);
+if (asnwer.operator === "Addition (+)") {
+    console.log(`Result is = ${asnwer.FirstNumber + asnwer.SecondNumber}`);
+}
+else if (asnwer.operator === "Subtraction (-)") {
+    console.log(`Result is = ${asnwer.FirstNumber - asnwer.SecondNumber}`);
+}
+else if (asnwer.operator === "Multiplication (x)") {
+    console.log(`Result is = ${asnwer.FirstNumber * asnwer.SecondNumber}`);
+}
+else if (asnwer.operator === "Division (÷)") {
+    console.log(`Result is = ${asnwer.FirstNumber / asnwer.SecondNumber}`);
+}
+else if (asnwer.operator === "Modulus (%)") {
+    console.log(`Result is = ${asnwer.FirstNumber % asnwer.SecondNumber}`);
+}
+else if (asnwer.operator === "Exponentiation (xx)") {
+    console.log(`Result is = ${asnwer.FirstNumber ** asnwer.SecondNumber}`);
+}
+else {
+    console.log("Please select vaild operator");
+}
